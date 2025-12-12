bb98723cece0451b8cb054591f97470d891d6edc
atepginzo <70021476+atepginzo@users.noreply.github.com>
2025-12-12 21:25:52 +0700

    time

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Paksa Waktu Indonesia Barat (WIB)
+        config(['app.timezone' => 'Asia/Jakarta']);
+        date_default_timezone_set('Asia/Jakarta');
+
+        // Set locale Indonesia untuk Carbon
+        \Carbon\Carbon::setLocale('id');
+
         // Paksa HTTPS jika di Production (Railway)
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
